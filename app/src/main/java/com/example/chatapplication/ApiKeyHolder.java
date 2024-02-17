13ca4c2981fb4dabb178e96377489a3ef9f10fcf
Sharain7 <sharainlmc7@gmail.com>
2024-02-17 18:26:09 +0530

    fixed the firebase security leak

--- app/src/main/java/com/example/chatapplication/ApiKeyHolder.java
+++ app/src/main/java/com/example/chatapplication/ApiKeyHolder.java
@@ -1,2 +1,11 @@
-package com.example.chatapplication;public class ApiKeyHolder {
+package com.example.chatapplication;
+
+public class ApiKeyHolder {
+
+    private static final String API_KEY = "AAAA5VYV75M:APA91bHin7PsmvAHp34tnuEVwHv3LinSi0IcyWnD3jA77-UDfEU5cOJxsUchRzE0tIhcnqnQnhbLl9Tbm22CpsS_mreFMEFgQBFUUsynHh-x6538rQWcuHwAEV5UIdmp-k6Pq2gedQcL";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
 }
+
--- app/src/main/java/com/example/chatapplication/ChatActivity.java
+++ app/src/main/java/com/example/chatapplication/ChatActivity.java
@@ -194,7 +194,7 @@ public class ChatActivity extends AppCompatActivity {
         Request request = new Request.Builder()
                 .url(url)
                 .post(requestBody)
-                .header("Authorization" , "Bearer AAAA5VYV75M:APA91bHin7PsmvAHp34tnuEVwHv3LinSi0IcyWnD3jA77-UDfEU5cOJxsUchRzE0tIhcnqnQnhbLl9Tbm22CpsS_mreFMEFgQBFUUsynHh-x6538rQWcuHwAEV5UIdmp-k6Pq2gedQcL")
+                .header("Authorization" , "Bearer " + ApiKeyHolder.getApiKey())
                 .build();
         client.newCall(request).enqueue(new Callback() {
             @Override
